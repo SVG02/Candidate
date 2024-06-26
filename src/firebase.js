777e89ffcb5ac14e275a841002e44a3016c7187d
Swarangi <89372069+SVG02@users.noreply.github.com>
2024-06-26 23:32:38 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -11,15 +11,5 @@ const app = firebase.initializeApp({
   measurementId: "G-R2EK3JBR5K"
 })
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDc3tnL2Orz2D1VEqpPGfSAxs9pl2n-xZ4",
-//   authDomain: "fir-e37dc.firebaseapp.com",
-//   projectId: "fir-e37dc",
-//   storageBucket: "fir-e37dc.appspot.com",
-//   messagingSenderId: "844241449581",
-//   appId: "1:844241449581:web:bbe2c611a4a387f16bc564",
-//   measurementId: "G-CLJX96CB5Q"
-// };
-
 export const auth = app.auth()
 export default app
